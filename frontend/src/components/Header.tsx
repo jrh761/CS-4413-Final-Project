784aa0e6a36ebc8c45b3a661f8dfea1a5e5432a2
bagoferasers <ctrull60@gmail.com>
2023-11-28 15:41:12 -0700

    Added init css to Header and Home.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,27 +1,21 @@
 import React from "react";
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Button, ButtonGroup } from "react-bootstrap";
+import "./Header.css";
 
 const Header: React.FC = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Container className="HeaderContainer">
+        <Navbar.Brand className="NavbarBrand">Social Media App</Navbar.Brand>
+        <Navbar.Toggle/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <ButtonGroup className="ButtonGroup">
+              <Button className="HeaderButton" href="#home">Feed</Button>
+              <Button className="HeaderButton">Profile</Button>
+              <Button className="HeaderButton">Register</Button>
+              <Button className="HeaderButton">Login</Button>
+            </ButtonGroup>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "./Home.css";
 
 const HomePage: React.FC = () => {
-  return <div>Home</div>;
+  return (
+  <div>
+    <Container className="HomeContainer">
+      <div className="ContentDiv">
+        <header>This is the Home page!</header>
+      </div>
+    </Container>
+  </div>
+  );
 };
 
 export default HomePage;
