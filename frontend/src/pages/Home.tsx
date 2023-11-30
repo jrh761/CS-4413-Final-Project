169abc5eeb7b2be9302eab8b26957dc533ab1db6
bagoferasers <ctrull60@gmail.com>
2023-11-29 20:31:46 -0700

    Profile?

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import "./Home.css";
+import PostCard from "../components/PostCard";
 
 const HomePage: React.FC = () => {
   return (
   <div>
     <Container className="HomeContainer">
-      <div className="ContentDiv">
-        <header>This is the Home page!</header>
-      </div>
+      <body className="HomeBodyDiv">
+        <main>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+          <PostCard></PostCard>
+        </main>
+      </body>
     </Container>
   </div>
   );
